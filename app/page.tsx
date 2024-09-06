ec39ec07fff4a8d84979e595af63005e50b2ccd7
NwobiaDavid <dnwobia@gmail.com>
2024-09-06 02:27:38 +0100

    feat: done with the footer

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
       <Projects />
       <OurProcess />
       {/* <Mission /> */}
-      {/* <Resources />
-      <Footer/> */}
+      <Resources />
+      <Footer/>
     </main>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
 
 
     return (
-        <div className=' bg-slate-950 h-[70vh] text-white w-full relative ' >
-            <div className="absolute left-[10%] opacity-70 top-[10%] ">
+        <div className=' bg-slate-950  text-white w-full relative ' >
+            <div className="absolute hidden lg:static left-[10%] opacity-70 top-[10%] ">
               <div className=' w-[100px] lg:w-[300px] gap-4 p-4 flex flex-col h-[100px] lg:h-[300px] ' >
                 <div className='w-full h-[60%] flex gap-4 ' >
                     <div className="bg-blue-400 h-full rounded-md w-[70%] "></div>
@@ -26,7 +26,7 @@ const Footer = () => {
                 <div className="flex justify-center items-center w-full h-[80%] ">
                     <div className=' w-full lg:w-[60%] text-center flex p-5 flex-col justify-center items-center mb-5 ' >
                         <h2 className=' font-bold opacity-70 text-xl ' >TheBluePixelAgency</h2>
-                        <h1 className=' text-2xl lg:text-5xl mt-10  ' >As your partner, we treat your website like it's our own</h1>
+                        <h1 className=' text-5xl mt-10  ' >As your partner, we treat your website like it's our own</h1>
                         <h4 className=' text-lg my-10 ' >Let’s find out if we’re a good fit for each other.</h4>
                         <button className=' px-10 py-5 flex items-center justify-between text-black bg-blue-400 hover:bg-blue-500 duration-200  rounded-md ' >
                             <FaRocket />
@@ -35,11 +35,11 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className=' w-full h-[20%] flex justify-center items-center ' >
+                <div className=' w-full h-[20%] mt-10 lg:mt-0 py-5 lg:py-0 flex justify-center items-center ' >
 
-                    <div className=' w-full flex justify-between border-t py-5 ' >
+                    <div className=' w-full lg:text-base text-center leading-6 flex-col flex justify-between border-t py-5 ' >
                         <span>Copyright © The Blue Pixel Agency Inc.</span>
-                        <span>Privacy Policy</span>
+                        <span className=' my-3 lg:my-0 ' >Privacy Policy</span>
                         <span>Based in Lagos, Ogun & Ebonyi</span>
                     </div>
                 </div>
